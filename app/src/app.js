30ba992c6bf83c76e5409ab10abd629abf49fcb8
b5710546232 <nattapat.su@ku.th>
2018-03-23 17:50:55 +0700

    add api-docs

--- app/src/app.js
+++ app/src/app.js
@@ -12,22 +12,13 @@ const fs = require('fs')
 const path = require('path')
 const swaggerDocument = YAML.load(path.join(__dirname,'/swagger.yaml'))
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next()
-})
-
-
 const db = require('./db')
 const port = process.env.PORT || 8080
 const VERSION = process.env.VERSION || '1'
 app.use(cors())
 app.use(bodyParser.json())
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.use(`/v${VERSION}`, routes)
 
