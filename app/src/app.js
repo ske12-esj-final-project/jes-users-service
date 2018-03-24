04584db93c907158b7440f3826506ee64d34d64c
b5710546232 <nattapat.su@ku.th>
2018-03-24 19:01:23 +0700

    fix app.js

--- app/src/app.js
+++ app/src/app.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
-    res.header('Access-Control-Allow-Headers', 'acess_token');
+    res.header('Access-Control-Allow-Headers', 'access_token');
     next()
 })
 
